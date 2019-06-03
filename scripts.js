bcef235f2f59eee03105f0b3362bb708f530d8f6
Shizz <wojciechbajko@gmail.com>
2019-06-03 22:32:10 +0200

    03.06.2019

--- scripts.js
+++ scripts.js
@@ -5,15 +5,19 @@ const btnMenu = document.querySelector('.btn-menu');
 const menuSmall = document.querySelector('.small-menu');
 const menuSmallList = document.querySelectorAll('.small-menu li');
 const menuBigList = document.querySelectorAll('.big-menu a');
+const allBody = document.querySelector('body');
+const header = document.querySelector('.portfolio-header');
 btnMenu.addEventListener('click', function(){
     menuSmall.classList.toggle('unvismenu');
-    btnMenu.classList.toggle('unvismenu');
+    allBody.classList.toggle('menu-body');
+    header.classList.toggle('unvismenu');
 });
 
 for(let i = 0; i<menuSmallList.length; i++){
     menuSmallList[i].addEventListener('click', function(){
         menuSmall.classList.toggle('unvismenu');
-        btnMenu.classList.toggle('unvismenu');
+        allBody.classList.toggle('menu-body');
+        header.classList.toggle('unvismenu');
     });
 }
 
@@ -24,5 +28,3 @@ for(let j = 0; j<menuBigList.length; j++){
         menuBigList[j].classList.add('active');
     });
 }
-
-fitText(document.querySelector(".big-title"), 0.38);
