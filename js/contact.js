b1953993e9463e42dac73320a1f968d98b055931
Shizz74 <wojciechbajko@gmail.com>
2019-06-04 09:42:03 +0200

    04.06.2019

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+const email = document.querySelector(".email");
+const subject = document.querySelector(".subject");
+const message = document.querySelector(".message");
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    if(email.length > 5 && email.includes("@") && email.includes(".")){}
+    else{
+        e.preventDefault();
+    }
+    if(subject.length >= 2){}
+    else{
+        e.preventDefault();
+    }
+    if(message.length >= 10 ){}
+    else{
+        e.preventDefault();
+    }
+    this.submit();
+});
